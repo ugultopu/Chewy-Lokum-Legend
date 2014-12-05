d6b0ab064da97015f5b8fe3a1d69370d617bc803
ceteke <cemeteke@gmail.com>
2014-12-05 14:40:51 +0200

    merge

--- src/App/InformationBoard.java
+++ src/App/InformationBoard.java
@@ -16,7 +16,7 @@ public class InformationBoard extends JPanel {
 	private int currentScore;
 	private int goalScore = 1000000000;
 	private int currentLevel;
-	private int moves=2;
+	private int moves=99;
 	private static InformationBoard instance;
 
 	public InformationBoard(){
