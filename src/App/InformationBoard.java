2d4fa193cf5df63128db2285fa11107bbdd1fc2d
ceteke <cemeteke@gmail.com>
2014-12-05 11:08:17 +0200

    Striped lokum merge fix.

--- src/App/InformationBoard.java
+++ src/App/InformationBoard.java
@@ -11,7 +11,7 @@ public class InformationBoard extends JPanel {
 	private JLabel level,target,score,movesLeft,levelInput,targetInput,scoreInput,movesLeftInput;
 	private JButton saveButton;
 	private int currentScore;
-	private int goalScore = 30000;
+	private int goalScore = 1000000000;
 	private int currentLevel;
 	private int moves=20;
 	private static InformationBoard instance;
