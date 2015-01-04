e4b61679259cdf709d029aebc846d9d41419ab48
ceteke <cemeteke@gmail.com>
2015-01-05 00:08:48 +0200

    Added targetscore to Options.

--- src/App/Options.java
+++ src/App/Options.java
@@ -3,11 +3,20 @@ package App;
 public class Options {
 	
 	public static int currentLevel = 1;
+	public static int targetScore = 1;
 	
 	private static Options instance;
 	
 	public void setLevel(int level){
 		currentLevel = level;
+	}
+	
+	public void setTargetScore(int score){
+		targetScore = score;
+	}
+	
+	public int getTargetScore(){
+		return targetScore;
 	}
 	
 	public int getLevel(){
