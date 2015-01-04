b606c47aadcef8c6142e1e36f643eeb6a8302aee
ceteke <cemeteke@gmail.com>
2015-01-04 20:57:06 +0200

    getGoalScore() method added to score, movelevel sends win or lose events.

--- src/App/MoveLevel.java
+++ src/App/MoveLevel.java
@@ -3,7 +3,7 @@ package App;
 
 public class MoveLevel extends Level {
 	private int movesLeft;
-	private MoveLevel instance;
+	private static MoveLevel instance;
 
 	private MoveLevel(int moves){
 		this.movesLeft = moves;
@@ -13,19 +13,29 @@ public class MoveLevel extends Level {
 		movesLeft--;
 		MoveUpdateEvent mue = new MoveUpdateEvent(movesLeft);
 		mue.executeEvent();
+		if(movesLeft == 0){
+			if(Score.getInstance().getCurrentScore() < Score.getInstance().getGoalScore(Options.currentLevel)){
+				LoseGameEvent lge = new LoseGameEvent();
+				lge.executeEvent();
+			}else{
+				WinGameEvent wge = new WinGameEvent();
+				wge.executeEvent();
+			}
+		}
+		
 	}
 	
 	public int getMovesLeft(){
 		return movesLeft;
 	}
 	
-	public MoveLevel getInstance(){
+	public static MoveLevel getInstance(){
 		if(instance == null)
 			instance = new MoveLevel(50 - (3 * Options.currentLevel));
 		return instance;
 	}
 	
-	public void resetInstance(){
+	public static void resetInstance(){
 		instance = new MoveLevel(50 - (3 * Options.currentLevel));
 	}
 }
