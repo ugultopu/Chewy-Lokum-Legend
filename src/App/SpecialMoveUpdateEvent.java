8111b32dce03ec0e3ac99db55e637bd325b303d2
Kaan <kugurlu94@gmail.com>
2015-01-05 00:18:26 +0200

    empty special move

---
+++ src/App/SpecialMoveUpdateEvent.java
@@ -0,0 +1,17 @@
+package App;
+
+public class SpecialMoveUpdateEvent extends GameEvent {
+	
+	private int newSpecialMoves;
+	
+	public SpecialMoveUpdateEvent(int newSpecialMoves){
+		
+	}
+
+	@Override
+	public void executeEvent() {
+		
+
+	}
+
+}
